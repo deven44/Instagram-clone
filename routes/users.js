6034d720e8d602a64dc054d8dbafb489d9c5d899
deven44 <119482297+deven44@users.noreply.github.com>
2024-03-08 20:26:20 +0530

    database changed to mongodb atlas

--- routes/users.js
+++ routes/users.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 const plm = require("passport-local-mongoose");
 
-mongoose.connect("mongodb://127.0.0.1:27017/instainsta");
+mongoose.connect("mongodb+srv://devenmewada660:JRscLvA99gPJrbIw@instacluster.wl7k1gz.mongodb.net/?retryWrites=true&w=majority&appName=instacluster");
 
 const userSchema = mongoose.Schema({
   username: String,
